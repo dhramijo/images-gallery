c10094776da465c1bacd42580ec89e0ff02e0c39
Jonad Dhrami <dhrami86@yahoo.it>
2022-09-09 19:29:21 +0100

    initial header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = ({ title }) => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">{title}</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;

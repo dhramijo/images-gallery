434fc263a896a9a7fe28e16979a0999ddc5596ae
Jonad Dhrami <dhrami86@yahoo.it>
2022-09-10 11:47:43 +0100

    first api call to unsplash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_API_URL = 'https://api.unsplash.com';
+
 const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
     console.log(word);
+    fetch(
+      `${UNSPLASH_API_URL}/photos/random/?query=${word}&client_id=${UNSPLASH_API_KEY}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (

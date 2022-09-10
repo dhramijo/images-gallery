a9cae1d2b1e2595f3ee87021a2daaf3327c4f2fe
Jonad Dhrami <dhrami86@yahoo.it>
2022-09-10 22:35:47 +0100

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const UNSPLASH_API_URL = 'https://api.unsplash.com';
@@ -10,17 +12,14 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images);
-
   const handleSearchSubmit = (event) => {
     event.preventDefault();
-    console.log(word);
     fetch(
       `${UNSPLASH_API_URL}/photos/random/?query=${word}&client_id=${UNSPLASH_API_KEY}`
     )
       .then((result) => result.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -29,10 +28,23 @@ const App = () => {
     setWord('');
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, index) => (
+            <Col className="pb-3" key={index}>
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ frontend/src/components/ImageCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+const ImageCard = ({ image, deleteImage }) => {
+  return (
+    <Card style={{ width: '22rem' }}>
+      <Card.Img
+        style={{ height: '18rem' }}
+        variant="top"
+        src={image.urls.small}
+      />
+      <Card.Body>
+        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Text style={{ width: '100%', height: '5rem' }}>
+          {image.description || image.alt_description}
+        </Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ImageCard;
